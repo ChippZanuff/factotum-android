8c03303c99159fd3a80e03259967742c05fe0a06
ChippZanuff <zanuff32167@mail.ru>
2016-11-19 23:27:04 +0200

    Created main visual concept for Home Page Activity

--- app/src/main/java/dah/budgetapp/Activities/MainActivity.java
+++ app/src/main/java/dah/budgetapp/Activities/MainActivity.java
@@ -4,15 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
+import com.intrusoft.scatter.ChartData;
+import com.intrusoft.scatter.PieChart;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dah.budgetapp.Categories.Category;
 import dah.budgetapp.R;
 
 public class MainActivity extends AppCompatActivity
 {
-    private Button newExpense;
-    private Button statistics;
-    private Button categories;
+    private ImageButton newExpense;
+    private ImageButton statistics;
+    private ImageButton categories;
+    private PieChart chart;
+    private float[] yData = { 5, 10, 15, 30, 40 };
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,8 +29,22 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         this.viewsFinder();
+
+        this.pieChart();
+    }
+
+    private void pieChart()
+    {
+        ArrayList<Category> categories = new ArrayList<>();
+        List<ChartData> data = new ArrayList<>();
+
+        for(int i = 0; i < categories.size(); i++)
+        {
+            data.add(new ChartData(categories.get(i).getName(), yData[i]));
+        }
+
+        this.chart.setChartData(data);
     }
 
     public void onClick(View view)
@@ -43,8 +66,9 @@ public class MainActivity extends AppCompatActivity
 
     private void viewsFinder()
     {
-        this.newExpense = (Button) findViewById(R.id.goto_new_expense);
-        this.statistics = (Button) findViewById(R.id.goto_statistics);
-        this.categories = (Button) findViewById(R.id.goto_categories);
+        this.newExpense = (ImageButton) findViewById(R.id.goto_new_expense);
+        this.statistics = (ImageButton) findViewById(R.id.goto_statistics);
+        this.categories = (ImageButton) findViewById(R.id.goto_categories);
+        this.chart = (PieChart) findViewById(R.id.pie_chart);
     }
 }
