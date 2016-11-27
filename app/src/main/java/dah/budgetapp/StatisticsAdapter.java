dfdd4ea8b61fa17dacf5611d3df8a41d47ceef2d
ChippZanuff <zanuff32167@mail.ru>
2016-11-27 22:16:34 +0200

    Deleted popup menu, added text view with sum

--- app/src/main/java/dah/budgetapp/StatisticsAdapter.java
+++ app/src/main/java/dah/budgetapp/StatisticsAdapter.java
@@ -1,13 +1,10 @@
 package dah.budgetapp;
 
 import android.content.Context;
-import android.support.v7.widget.PopupMenu;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.List;
@@ -60,38 +57,6 @@ public class StatisticsAdapter extends BaseAdapter
 
         ((TextView) view.findViewById(R.id.name_statistics)).setText(category.getName());
         ((TextView) view.findViewById(R.id.sum_statistics)).setText(Integer.toString(10));
-
-        ImageButton menu = (ImageButton) view.findViewById(R.id.popup_menu);
-
-        menu.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                PopupMenu popupMenu = new PopupMenu(context, view);
-                popupMenu.getMenuInflater().inflate(R.menu.item_settings_menu, popupMenu.getMenu());
-                popupMenu.show();
-                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener()
-                {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem item)
-                    {
-                        switch (item.getItemId())
-                        {
-                            case R.id.edit:
-
-                                break;
-                            case R.id.delete:
-
-                                break;
-                        }
-
-                        return true;
-                    }
-                });
-            }
-        });
-
         return view;
     }
 }
